2c266c04e81942e0e9f3e4ca727eb236d7786d0c
amine-el-halili <amine.elhalili1@usmba.ac.ma>
2022-11-22 11:22:28 +0100

    Rename/refactor classes

---
+++ Qcm.cs
@@ -0,0 +1,18 @@
+﻿namespace Session2
+{
+    class Qcm : Question
+    {
+        private List<Suggestion> suggestions;
+        private float initialScore { get; set; }
+
+        public Qcm(float score)
+        {
+            initialScore = score;
+        }
+        public void addSuggestion(Suggestion sg)
+        {
+            suggestions.Add(sg);
+        }
+        
+    }
+}
---
+++ ResponsePaper.cs
@@ -0,0 +1,14 @@
+﻿namespace Session2
+{
+    class ResponsePaper
+    {
+        private Candidate candidate { get; set; }
+        private Exam exam { get; set; }
+        private List<Response> responses;
+
+        public void addResponse(Response response)
+        {
+            responses.Add(response);
+        }
+    }
+}
