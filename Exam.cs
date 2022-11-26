df008d23b4c2cfd12a613eb7aa5d465a99c15cd3
amine-el-halili <amine.elhalili1@usmba.ac.ma>
2022-11-26 17:19:28 +0100

    Add a new attribute

--- Exam.cs
+++ Exam.cs
@@ -5,6 +5,7 @@
         private string id { get; set; }
         private string title { get; set; }
         private DateTime openedAt { get; set; }
+        public DateTime closedAt { get; set; }
         private int duration { get; set; }
         private List<Question> questions;
         public void addQuestion(Question question)
