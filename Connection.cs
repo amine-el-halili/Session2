b68d70dd495bc8ad45b77dc1e6843d9e3c73e338
amine-el-halili <amine.elhalili1@usmba.ac.ma>
2022-11-27 13:09:30 +0100

    Rename

---
+++ Connection.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Session2
+{
+    internal class Connection
+    {
+        private static string str;
+
+        private static SqlConnection cnx;
+        private static SqlCommand cmd;
+        private static SqlDataAdapter adapter;
+
+        public static void init(string cnx_str)
+        {
+            str = cnx_str;
+            SqlConnection cnx = new SqlConnection(str);
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        }
+
+        public static DataTable query(string qr)
+        {
+
+            cnx.Open();
+            cmd.CommandText = str;
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            cnx.Close();
+
+            return dt;
+        }
+
+    }
+}
