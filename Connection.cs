a6a1893716ec4f177d6ff2eb0f3783b1b61283d5
amine-el-halili <amine.elhalili1@usmba.ac.ma>
2022-11-27 21:35:11 +0100

    Refactor: implement Singelton design pattern

--- Connection.cs
+++ Connection.cs
@@ -3,32 +3,44 @@ using System.Data.SqlClient;
 
 namespace Session2
 {
+    // implement Singelton design patterns
     internal class Connection
     {
-        private static string str;
+        private SqlConnection cnx;
+        private SqlCommand cmd;
+        private SqlDataAdapter adapter;
 
-        private static SqlConnection cnx;
-        private static SqlCommand cmd;
-        private static SqlDataAdapter adapter;
-
-        public static void init(string cnx_str)
+        private Connection(string cnx_str)
         {
-            str = cnx_str;
-            SqlConnection cnx = new SqlConnection(str);
-            SqlCommand cmd = new SqlCommand();
-            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            cnx = new SqlConnection(cnx_str);
+            cmd = new SqlCommand();
+            adapter = new SqlDataAdapter(cmd);
         }
 
-        public static DataTable query(string qr)
+        public DataTable query(string qr)
         {
-
             cnx.Open();
-            cmd.CommandText = str;
+            cmd.Connection = cnx;
+            cmd.CommandText = qr;
             DataTable dt = new DataTable();
             adapter.Fill(dt);
             cnx.Close();
 
             return dt;
+        }
+
+        private static Connection instance;
+        public static Connection getInstance(string cnx_str)
+        {
+            if (instance == null)
+            {
+                instance = new Connection(cnx_str);
+
+                return instance;
+            } else
+            {
+                return instance;
+            }
         }
 
     }
