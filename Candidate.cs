8f2a53b0627ba1bab09ab79faa8c3a25065fd34d
amine-el-halili <amine.elhalili1@usmba.ac.ma>
2022-11-27 21:36:32 +0100

    Refactor/Add:

--- Candidate.cs
+++ Candidate.cs
@@ -1,13 +1,47 @@
-﻿
+﻿using System.Data;
+
 namespace Session2
 {
     class Candidate
-    {
-        public int id { get; set; } 
-        private string name
+    {   
+        private int id;
+        public int Id {
+            get { return id; }
+            set { id = value; }
+         }
+        private string name;
+        public string Name
         {
-            get;
-            set;
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Candidate(string name)
+        {
+            this.name = name;
+        }
+        public void save()
+        {
+            try
+            {
+                Connection connection = Connection.getInstance(@"Data Source=ELHALILI\SQLEXPRESS;Initial Catalog=EXAMINATION;Integrated Security=True");
+                DataTable dt = connection.query("Select count(*) as count from Candidat where ID = " + id);
+
+                if (int.Parse(dt.Rows[0]["count"].ToString()) == 1)
+                {
+                    connection.query("UPDATE CANDIDAT SET NAME = '" + name + "' WHERE ID = " + id);
+                }
+                else
+                {
+                    connection.query("INSERT INTO CANDIDAT (NAME) VALUES ('" + name + "');");
+                }
+            } catch (Exception ex)
+            {
+                ErrorForm error = new ErrorForm();
+                error.Controls[0].Text = ex.Message;
+                error.Show();
+            }
+
         }
     }
 }
---
+++ ErrorForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Session2
+{
+    partial class ErrorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label.Location = new System.Drawing.Point(160, 104);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(78, 32);
+            this.label.TabIndex = 0;
+            this.label.Text = "label1";
+            // 
+            // ErrorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(395, 272);
+            this.Controls.Add(this.label);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Name = "ErrorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ErrorForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label;
+    }
+}
