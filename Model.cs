f674d33bdc6230490e8b8c15072a6e845547d1d9
amine-el-halili <amine.elhalili1@usmba.ac.ma>
2022-11-27 11:46:42 +0100

    Add Model.cs :  responsable for handling database stuffs

---
+++ Model.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Session2
+{
+    internal class Model
+    {
+        private string str;
+
+        private SqlConnection cnx;
+        private SqlCommand cmd;
+        private SqlDataAdapter adapter;
+
+        public Model(string str)
+        {
+            this.str = str;
+            SqlConnection cnx = new SqlConnection(str);
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        }
+
+
+    }
+}
