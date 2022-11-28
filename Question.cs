dc0b87da6b41bec6806ca72a132f663c6d4f6af6
amine-el-halili <amine.elhalili1@usmba.ac.ma>
2022-11-28 15:20:34 +0100

    Add constructor and "save" abstract method

--- Question.cs
+++ Question.cs
@@ -2,20 +2,41 @@
 {
     abstract class Question
     {
-        protected string id
+        protected int id
         {
             get;
             set;
+        }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
         }
         protected string statement
         {
             get;
             set;
         }
+        public string Statement
+        {
+            get { return statement; }
+            set { statement = value; }
+        }
         protected float note
         {
             get;
             set;
         }
+        public float Note
+        {
+            get { return note; }
+            set { note = value; }
+        }
+        public Question(string _statement)
+        {
+            this.statement = _statement;
+        }
+        public abstract Question save();
+
     }
 }
