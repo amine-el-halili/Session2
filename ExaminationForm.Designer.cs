7deddd00d7559600b2c7fb36aabe60a710c71165
amine-el-halili <amine.elhalili1@usmba.ac.ma>
2022-11-28 18:28:05 +0100

    Add new features: pop up form and list the exist exams

--- ExaminationForm.Designer.cs
+++ ExaminationForm.Designer.cs
@@ -3,9 +3,7 @@
     partial class ExaminationForm
     {
         private List<Exam> exams;
-        private Exam exam;
-        private List<Question> questions;
-        private List<Suggestion> suggestions;
+
 
         /// <summary>
         /// Required designer variable.
@@ -24,7 +22,6 @@
             }
             base.Dispose(disposing);
         }
-
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -33,23 +30,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_createExam = new System.Windows.Forms.Button();
+            this.cmbox_exams = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btn_createExam
+            // 
+            this.btn_createExam.Location = new System.Drawing.Point(430, 122);
+            this.btn_createExam.Name = "btn_createExam";
+            this.btn_createExam.Size = new System.Drawing.Size(138, 29);
+            this.btn_createExam.TabIndex = 0;
+            this.btn_createExam.Text = "Create exam";
+            this.btn_createExam.UseVisualStyleBackColor = true;
+            this.btn_createExam.Click += new System.EventHandler(this.btn_createExam_Click);
+            // 
+            // cmbox_exams
+            // 
+            this.cmbox_exams.FormattingEnabled = true;
+            this.cmbox_exams.Location = new System.Drawing.Point(182, 123);
+            this.cmbox_exams.Name = "cmbox_exams";
+            this.cmbox_exams.Size = new System.Drawing.Size(151, 28);
+            this.cmbox_exams.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "list of exams";
             // 
             // ExaminationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(722, 329);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbox_exams);
+            this.Controls.Add(this.btn_createExam);
             this.Name = "ExaminationForm";
             this.Text = "Initial";
+            this.Load += new System.EventHandler(this.ExaminationForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
+
         #endregion
 
-        private void initial_state()
-        {
-
-        }
-
+        private Button btn_createExam;
+        private ComboBox cmbox_exams;
+        private Label label1;
     }
 }
--- ExaminationForm.cs
+++ ExaminationForm.cs
@@ -6,5 +6,23 @@
         {
             InitializeComponent();
         }
+
+        public void loadListExams()
+        {
+            cmbox_exams.DataSource = Exam.getALl();
+            cmbox_exams.DisplayMember = "title";
+            cmbox_exams.ValueMember =  "id";
+        }
+        private void btn_createExam_Click(object sender, EventArgs e)
+        {
+            var createExamForm = new CreateExamForm();
+            createExamForm.Show(this);
+            this.Enabled = false;
+        }
+
+        private void ExaminationForm_Load(object sender, EventArgs e)
+        {
+            loadListExams();
+        }
     }
 }
